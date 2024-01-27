3c47f559c49ba9ea314a098671d2d13ac0871133
BrunoAneves <araujobruno20@gmail.com>
2024-01-27 09:27:41 -0300

    adicionando funcionalidades da page3

--- script/script.js
+++ script/script.js
@@ -4,27 +4,31 @@ const check = document.getElementById('check')
 const span = document.getElementById('header_span')
 const colorText = document.querySelectorAll('.textBlack')
 
-check.addEventListener('change', () => {
-    document.body.classList.toggle('white')
 
+check.addEventListener('change', () => {
+
+    document.body.classList.toggle('white')
+   
 
     colorText.forEach(element => {
         element.classList.toggle('textBlack')
     })
-
-
+    
+    
     if (span.innerText == 'Tema Escuro') {
         span.style.color = '#535353'
         span.innerText = 'Tema Claro'
-
-
+        
+        
     } else {
         span.innerText = 'Tema Escuro'
         span.style.color = '#bbbbbb'
-
+        
     }
-
+   
+    
 })
+
 
 
 
@@ -37,7 +41,7 @@ const erroEmail = document.getElementById('erroEmail')
 const erroCep = document.getElementById('erroCep')
 const page1 = document.getElementById('page1')
 const page2 = document.getElementById('page2')
-
+const main_text = document.querySelector('.main_text')
 
 function isValidEmail(email) {
     return /\S+@\S+\.\S+/.test(email);
@@ -77,6 +81,7 @@ btnPage1.addEventListener('click', () => {
     if (!hasErrors) {
         page1.style.display = 'none'
         page2.style.display = 'block'
+        main_text.innerText = 'Precisa de uma ajudinha com o churrasco? :)' + " " + 'Quantas pessoas vão participar?'
     }
 
 })
@@ -104,6 +109,130 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     })
 })
+
+
+
+
+// Calcular itens do churrasco 
+
+const buttonCalcular = document.getElementById('buttonCalcular')
+const buttonNovoCalc = document.getElementById('buttonNovoCalc')
+
+
+
+buttonCalcular.addEventListener('click', () => {
+    calcItems()
+})
+
+
+
+buttonNovoCalc.addEventListener('click', () => {
+    document.getElementById('page2').style.display = 'block';
+    document.getElementById('page3').style.display = 'none';
+    
+})
+
+
+
+function calcItems() {
+    const men = parseInt(document.getElementById('men').value);
+    const women = parseInt(document.getElementById('women').value);
+    const children = parseInt(document.getElementById('children').value);
+    const drinkers = parseInt(document.getElementById('drinkers').value);
+
+
+
+    const meatQuantity = 0.4 * men + 0.32 * women + 0.2 * children;
+    const garlicBreadQuantity = 2 * (men + women) + 1 * children;
+    const charcoalQuantity = 1 * (men + women + children + drinkers);
+    const saltQuantity = 0.04 * (men + women + children + drinkers);
+    const iceQuantity = 5 * Math.ceil((men + women + children + drinkers) / 10);
+    const sodaQuantity = Math.ceil((men + women + children + drinkers) / 5);
+    const waterQuantity = Math.ceil((men + women + children + drinkers) / 5);
+    const beerQuantity = 3 * drinkers;
+
+
+    const infosConvidados = document.getElementById('infosConvidados')
+    infosConvidados.innerHTML = ` <h3>${men+women+children} Convidados</h3>
+                                    <div class="div_convidados">
+                                        <span>${men} homen</span>
+                                        <span>${women} homen</span>
+                                        <span>${children} homen</span>
+                                    </div>`
+
+
+
+    const resultTable = document.getElementById('resultTable');
+    resultTable.innerHTML = `
+      <tr><td>Carne</td><td>${meatQuantity.toFixed(2)} KG</td></tr>
+      <tr><td>Pão de Alho</td><td>${garlicBreadQuantity}</td></tr>
+      <tr><td>Carvão</td><td>${charcoalQuantity} KG</td></tr>
+      <tr><td>Sal</td><td>${saltQuantity.toFixed(2)} KG</td></tr>
+      <tr><td>Gelo</td><td>${iceQuantity} KG</td></tr>
+      <tr><td>Refrigerante</td><td>${sodaQuantity} garrafas de 2L</td></tr>
+      <tr><td>Água</td><td>${waterQuantity} garrafas de 1L</td></tr>
+      <tr><td>Cerveja</td><td>${beerQuantity} garrafas de 600ml</td></tr>
+    `;
+
+
+
+
+
+    document.getElementById('page2').style.display = 'none';
+    document.getElementById('page3').style.display = 'block';
+    main_text.innerText = 'Confira a lista para o seu churrasco!'
+    
+}
+
+
+// function calculateItems() {
+//     // Obter quantidades do formulário
+//     const men = parseInt(document.getElementById('men').value);
+//     const women = parseInt(document.getElementById('women').value);
+//     const children = parseInt(document.getElementById('children').value);
+//     const drinkers = parseInt(document.getElementById('drinkers').value);
+    
+//     // Calcular quantidade de cada item conforme as regras
+//     const meatQuantity = 0.4 * men + 0.32 * women + 0.2 * children;
+//     const garlicBreadQuantity = 2 * (men + women) + 1 * children;
+//     const charcoalQuantity = 1 * (men + women + children + drinkers);
+//     const saltQuantity = 0.04 * (men + women + children + drinkers);
+//     const iceQuantity = 5 * Math.ceil((men + women + children + drinkers) / 10);
+//     const sodaQuantity = Math.ceil((men + women + children + drinkers) / 5);
+//     const waterQuantity = Math.ceil((men + women + children + drinkers) / 5);
+//     const beerQuantity = 3 * drinkers;
+    
+//     // Preencher a tabela de resultado
+//     const resultTable = document.getElementById('resultTable');
+//     resultTable.innerHTML = `
+//       <tr><td>Carne</td><td>${meatQuantity.toFixed(2)} KG</td></tr>
+//       <tr><td>Pão de Alho</td><td>${garlicBreadQuantity}</td></tr>
+//       <tr><td>Carvão</td><td>${charcoalQuantity} KG</td></tr>
+//       <tr><td>Sal</td><td>${saltQuantity.toFixed(2)} KG</td></tr>
+//       <tr><td>Gelo</td><td>${iceQuantity} KG</td></tr>
+//       <tr><td>Refrigerante</td><td>${sodaQuantity} garrafas de 2L</td></tr>
+//       <tr><td>Água</td><td>${waterQuantity} garrafas de 1L</td></tr>
+//       <tr><td>Cerveja</td><td>${beerQuantity} garrafas de 600ml</td></tr>
+//     `;
+    
+//     // Armazenar as quantidades no armazenamento do navegador
+//     localStorage.setItem('meatQuantity', meatQuantity);
+//     localStorage.setItem('garlicBreadQuantity', garlicBreadQuantity);
+//     localStorage.setItem('charcoalQuantity', charcoalQuantity);
+//     localStorage.setItem('saltQuantity', saltQuantity);
+//     localStorage.setItem('iceQuantity', iceQuantity);
+//     localStorage.setItem('sodaQuantity', sodaQuantity);
+//     localStorage.setItem('waterQuantity', waterQuantity);
+//     localStorage.setItem('beerQuantity', beerQuantity);
+    
+//     // Esconder passo atual e exibir próximo
+//     document.getElementById('step2').style.display = 'none';
+//     document.getElementById('step3').style.display = 'block';
+//     }
+
+  
+  
+
 
 
 
